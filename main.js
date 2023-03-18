2dc1c94ef7e22e98b836756164afb75061415b3c
Aziz <19191@.ru>
2023-03-19 03:20:10 +0500

    save

---
+++ main.js
@@ -0,0 +1,13 @@
+var mark = +prompt('Введите Вашу возраст')
+
+if(mark === 1 || mark === 2 || mark === 3 || mark === 3 || mark === 4 || mark === 5 || mark === 6 || mark === 7 || mark === 8 || mark === 9 || mark === 10 || mark === 11 || mark === 12 || mark === 13 || mark === 14 || mark === 15 || mark === 16 || mark === 18) {
+    alert('Вы ещё молоды, Вам нужно учиться');
+}else if(mark === 19 ||  mark === 20 || mark === 21 || mark === 22 || mark === 23 || mark === 24 || mark === 25 || mark === 26 || mark === 27 || mark === 28 || mark === 29 || mark === 30 || mark ===  31 || mark === 32 || mark === 33 || mark === 34 || mark === 35 || mark === 36 || mark === 37 || mark === 38 || mark === 39 || mark === 40 || mark === 41 || mark === 42 || mark === 43 || mark === 44 || mark === 45 || mark === 46 || mark === 47 || mark === 48 || mark === 49 || mark === 50) {
+    alert('Вам нужно работать');
+}else if(mark === 51 || mark === 52 || mark === 53 || mark === 54 || mark === 55 || mark === 56 || mark === 57 || mark === 58 || mark === 59) {
+    alert('Вам скоро на пенсию');
+}else if(mark === 60 || mark === 61 || mark === 62 || mark === 63 || mark === 64 || mark === 65 || mark === 66 || mark === 67 || mark === 68 || mark === 69 || mark === 70 || mark === 71 || mark === 72 || mark === 73 || mark === 74 || mark === 75 || mark === 76 || mark === 77 || mark === 78 || mark === 79 || mark === 80 || mark === 81 || mark === 82 || mark === 83 || mark === 84 || mark === 85 || mark === 86 || mark === 87 || mark === 88 || mark === 89 || mark === 90 || mark === 90 || mark === 91 || mark === 92 || mark === 93 || mark === 94 || mark === 95 || mark === 96 || mark === 97 || mark === 98 || mark === 99 || mark === 100) {
+    alert('Вы пенсионер')
+}else {
+    alert('Что-то пошло не так')
+}
